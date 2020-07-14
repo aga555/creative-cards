255a17727bced352f603cbc4cc5586fc1f383090
akrawczyk <agammkrawczyk@gmail.com>
2020-07-14 20:43:30 +0200

    #Registering global components

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.component('text-input', {
+  template: '<textarea :placeholder="textValue" @click="changeText"> </textarea:placeholder>',
+  data: function () {
+return {
+  textValue:'type here',
+
+}
+  },
+  methods:{
+    changeText: function () {
+      this.textValue ="Text changed"
+    }
+  }
+})
+
 new Vue({
   el: '#app',
-  render: h => h(App)
+  //render: h => h(App)
 })
