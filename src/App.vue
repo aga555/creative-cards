f48fd9114b820dbd57a395b45c814151f9082e48
akrawczyk <agammkrawczyk@gmail.com>
2020-07-15 19:58:25 +0200

    # Introduction to slots

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,20 @@
       <div class="col-sm-12">
         <nav-header @pageWsaChanged="currentPage = $event"></nav-header>
         <keep-alive>
-        <component :is="currentPage"></component> </keep-alive>
+        <component :is="currentPage"></component>
+        </keep-alive>
+        <footer>
+          <p class="text-center">Creative cards </p>
+          <nav>
+            <ul style="">
+
+              <li class="nav-item"><a class="nav-link"> Home </a></li>
+              <li class="nav-item"><a class="nav-link">About us </a></li>
+              <li class="nav-item"><a class="nav-link">Contact </a></li>
+
+            </ul>
+          </nav>
+        </footer>
       </div>
     </div>
   </div>
@@ -16,6 +29,7 @@
     import CardInsideLeft from "./components/card/CardInsideLeft";
     import CardInsideRight from "./components/card/CardInsideRight";
     import CardBack from "./components/card/CardBack";
+    import Footer from "./components/Footer";
 
     export default {
         data: function () {
@@ -29,7 +43,8 @@
             cardFront: CardFront,
             cardInsideLeft: CardInsideLeft,
             cardInsideRight: CardInsideRight,
-            cardBack:CardBack
+            cardBack:CardBack,
+            footer:Footer
         }
     }
 
---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer>
+    <slot></slot>
+  </footer>
+</template>
+
+<script>
+    export default {
+        name: "Footer"
+    }
+</script>
+
+<style scoped>
+
+</style>
