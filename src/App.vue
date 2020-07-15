a2d70ecb75b1de3779f5a9602da88f554f0d4a1a
akrawczyk <agammkrawczyk@gmail.com>
2020-07-15 12:22:26 +0200

    #styling

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,31 @@
 <template>
-<div class="container">
-  <div class="row">
-    <div class="col-sm-12">
-<nav-header></nav-header>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <nav-header></nav-header>
+        <card-front></card-front>
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
-import Header from "./components/Header";
-export default {
-    components: {
-        navHeader:Header
+    import Header from "./components/Header";
+    import CardFront from "./components/card/CardFront.vue";
+
+    export default {
+        components: {
+            navHeader: Header,
+            cardFront: CardFront
+        }
     }
-}
 
 
 </script>
 
 <style>
-
+body{
+  font-family: Verdana, Gneva,sans-serif;
+  color: #333;
+}
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,13 @@
 <template>
-    <div>
-      <h1 class="text-center">Creative Cards</h1>
-      <ul class="nav justify-content-center">
-        <li class="nav-item">Front </li>
-        <li class="nav-item">Inside left </li>
-        <li class="nav-item">Inside right </li>
-        <li class="nav-item">Back </li>
-      </ul>
-    </div>
+  <div>
+    <h1 class="text-center">Creative Cards</h1>
+    <ul class="nav justify-content-center">
+      <li class="nav-item"><a class="nav-link"> Front </a></li>
+      <li class="nav-item"><a class="nav-link">Inside left </a></li>
+      <li class="nav-item"><a class="nav-link">Inside right </a></li>
+      <li class="nav-item"><a class="nav-link">Back </a></li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -17,7 +17,11 @@
 </script>
 
 <style scoped>
-h1{
-  margin: 20px;
-}
+  h1 {
+    margin: 20px;
+  }
+
+  a {
+    cursor: pointer
+  }
 </style>
