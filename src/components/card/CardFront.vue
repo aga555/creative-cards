01b16b4937ba4dcc4c7dfb79546ae770fa70b322
akrawczyk <agammkrawczyk@gmail.com>
2020-07-15 16:50:01 +0200

    #Receiving data from the text input 9 eventlistner comunication child to Parent

--- src/components/card/CardFront.vue
+++ src/components/card/CardFront.vue
@@ -1,32 +1,44 @@
 <template>
   <div class="row">
     <div class="col-sm-6 card edit-area">
-      <cc-text-input></cc-text-input>
-      <cc-text-input></cc-text-input>
-      <cc-text-input></cc-text-input>
+      <cc-text-input @displayTextChanged="textBoxValue1= $event"></cc-text-input>
+      <cc-text-input @displayTextChanged="textBoxValue2= $event"></cc-text-input>
+      <cc-text-input @displayTextChanged="textBoxValue3=$event"></cc-text-input>
     </div>
+    1 :{{textBoxValue1}}
+    2 :{{textBoxValue2}}
+    3: {{textBoxValue3}}
     <div class="col-sm-6 card card-display"></div>
   </div>
 </template>
 
 <script>
-  import TextInput from "./TextInput";
+    import TextInput from "./TextInput";
+
     export default {
         name: "CardFront",
-        components:{
-            ccTextInput:TextInput
+        data: function () {
+            return {
+                textBoxValue1: '',
+                textBoxValue2: '',
+                textBoxValue3: '',
+            }
+        },
+        components: {
+            ccTextInput: TextInput
         }
     }
 </script>
 
 <style scoped>
 
-  .edit-area{
+  .edit-area {
     background-color: #d2f9f9;
     padding: 20px;
     height: 800px;
   }
-  .card-display{
+
+  .card-display {
     height: 800px;
     padding: 20px;
   }
