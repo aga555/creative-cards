480fbce8a823271176ee451c59313917131079b1
akrawczyk <agammkrawczyk@gmail.com>
2020-07-21 22:25:10 +0200

    #Finishing the CardBack component

--- src/components/card/CardBack.vue
+++ src/components/card/CardBack.vue
@@ -1,20 +1,47 @@
-
 <template>
   <div class="row">
-    <p> Card Back</p>
-    <div class="col-sm-6 card edit-area"></div>
-    <div class="col-sm-6 card card-display"></div>
+    <div class="col-sm-6 card edit-area">
+
+      <cc-image-upload @displayImageChanged="imageName=$event" ></cc-image-upload><hr>
+
+    </div>
+
+    <div class="col-sm-6 card card-display">
+
+      <cc-image-output :display-image="imageName" :container-height="400" :clearImageProp="clearImage"></cc-image-output>
+
+
+
+
+    </div>
+
   </div>
 </template>
 
 <script>
+
+    import ImageUpload from "./ImageUpload";
+    import ImageOutput from "./ImageOutput";
+
+
     export default {
-        name: "CardBack"
+        name: "CardBack",
+        data: function () {
+            return {
+
+                imageName :''
+            }
+        },
+        methods:{
+            clearImage:function () {
+                if ( this.imageName !==''){
+                    this.imageName='water-3322250_640.jpg'
+                } }
+        },
+        components: {
+
+            ccImageUpload: ImageUpload,
+            ccImageOutput: ImageOutput
+        }
     }
 </script>
-
-
-
-<style scoped>
-
-</style>
